9a1b4006694152a5733091161a3c2883612ee47b
malikakika <malikachoubri@gmail.com>
2025-05-05 22:06:38 +0200

    fix merge syntax

--- apps/my-acorad-front/src/app/app.tsx
+++ apps/my-acorad-front/src/app/app.tsx
@@ -1,15 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import LandingPage from '../components/landingPage';
-import Bootcamp from '../components/bootcampPage';
-import Footer from '../components/footer';
 import ConferencePage from '../components/conferencePage/conferencePage';
-=======
 import Bootcamp from '../components/bootcampPage/bootcampPage';
 import Footer from '../components/homePgae/footer';
 import LandingPage from '../components/homePgae/landingPage';
 import { CookieBanner } from '../components/cookieBanner';
->>>>>>> main
 
 const App = () => {
   return (
